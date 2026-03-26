c3bff392b9cdac0cee98ff49710974814354c889
Maura Pintor <maura.pintor@gmail.com>
2026-03-26 13:32:14 +0000

    fix issues in hubconf

--- hubconf.py
+++ hubconf.py
@@ -2,13 +2,18 @@ dependencies = ['torch']
 
 
 def mnist_model(student: bool = True):
+    import torch
     from model import MNISTModel
     model = MNISTModel()
     if student:
-        weigths_url = "https://github.com/maurapintor/distilled_mnist/releases/download/checkpoints/mnist_distilled.pt"
+        weights_url = "https://github.com/maurapintor/distilled_mnist/releases/download/checkpoints/mnist_distilled.pt"
     else:
-        weigths_url = "https://github.com/maurapintor/distilled_mnist/releases/download/checkpoints/mnist_teacher.pt"
-    state_dict = torch.hub.load_state_dict_from_url(weigths_url)
+        weights_url = "https://github.com/maurapintor/distilled_mnist/releases/download/checkpoints/mnist_teacher.pt"
+    state_dict = torch.hub.load_state_dict_from_url(
+        weights_url,
+        map_location=torch.device("cpu"),
+        weights_only=True,
+    )
     model.load_state_dict(state_dict)
     return model
 
